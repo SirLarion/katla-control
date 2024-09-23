bb04339b7d2c321003def47779c22d0ca66f6a7c
sirlarion <miska@tammenpaa.com>
2024-09-23 12:06:46 +0300

    feat: create simple index page

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,6 +1,7 @@
 // Sensor data collected by Katla. The values need to be nullable so as not to
 // invalidate the entire payload if one value is null
 export type KatlaSensorPayload = {
+  secret: string;
   temp_in?: number;
   temp_out?: number;
   humidity?: number;
