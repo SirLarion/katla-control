c7518077658277375a8ea0b084e42b5f79765ecb
sirlarion <miska@tammenpaa.com>
2024-09-24 08:19:37 +0300

    feat: establish e2e connectivity

---
+++ esp-request/esp-request.ino
@@ -0,0 +1,96 @@
+#include <HTTPClient.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+#include "secrets.h"
+
+const char *ssid = SECRET_SSID;
+const char *pwd = SECRET_PWD;
+const char *secret = CONTROLLER_SECRET;
+
+const char *cert = "-----BEGIN CERTIFICATE-----\n\
+MIIFBTCCAu2gAwIBAgIQS6hSk/eaL6JzBkuoBI110DANBgkqhkiG9w0BAQsFADBP\n\
+MQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJuZXQgU2VjdXJpdHkgUmVzZWFy\n\
+Y2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBYMTAeFw0yNDAzMTMwMDAwMDBa\n\
+Fw0yNzAzMTIyMzU5NTlaMDMxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBF\n\
+bmNyeXB0MQwwCgYDVQQDEwNSMTAwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\n\
+AoIBAQDPV+XmxFQS7bRH/sknWHZGUCiMHT6I3wWd1bUYKb3dtVq/+vbOo76vACFL\n\
+YlpaPAEvxVgD9on/jhFD68G14BQHlo9vH9fnuoE5CXVlt8KvGFs3Jijno/QHK20a\n\
+/6tYvJWuQP/py1fEtVt/eA0YYbwX51TGu0mRzW4Y0YCF7qZlNrx06rxQTOr8IfM4\n\
+FpOUurDTazgGzRYSespSdcitdrLCnF2YRVxvYXvGLe48E1KGAdlX5jgc3421H5KR\n\
+mudKHMxFqHJV8LDmowfs/acbZp4/SItxhHFYyTr6717yW0QrPHTnj7JHwQdqzZq3\n\
+DZb3EoEmUVQK7GH29/Xi8orIlQ2NAgMBAAGjgfgwgfUwDgYDVR0PAQH/BAQDAgGG\n\
+MB0GA1UdJQQWMBQGCCsGAQUFBwMCBggrBgEFBQcDATASBgNVHRMBAf8ECDAGAQH/\n\
+AgEAMB0GA1UdDgQWBBS7vMNHpeS8qcbDpHIMEI2iNeHI6DAfBgNVHSMEGDAWgBR5\n\
+tFnme7bl5AFzgAiIyBpY9umbbjAyBggrBgEFBQcBAQQmMCQwIgYIKwYBBQUHMAKG\n\
+Fmh0dHA6Ly94MS5pLmxlbmNyLm9yZy8wEwYDVR0gBAwwCjAIBgZngQwBAgEwJwYD\n\
+VR0fBCAwHjAcoBqgGIYWaHR0cDovL3gxLmMubGVuY3Iub3JnLzANBgkqhkiG9w0B\n\
+AQsFAAOCAgEAkrHnQTfreZ2B5s3iJeE6IOmQRJWjgVzPw139vaBw1bGWKCIL0vIo\n\
+zwzn1OZDjCQiHcFCktEJr59L9MhwTyAWsVrdAfYf+B9haxQnsHKNY67u4s5Lzzfd\n\
+u6PUzeetUK29v+PsPmI2cJkxp+iN3epi4hKu9ZzUPSwMqtCceb7qPVxEbpYxY1p9\n\
+1n5PJKBLBX9eb9LU6l8zSxPWV7bK3lG4XaMJgnT9x3ies7msFtpKK5bDtotij/l0\n\
+GaKeA97pb5uwD9KgWvaFXMIEt8jVTjLEvwRdvCn294GPDF08U8lAkIv7tghluaQh\n\
+1QnlE4SEN4LOECj8dsIGJXpGUk3aU3KkJz9icKy+aUgA+2cP21uh6NcDIS3XyfaZ\n\
+QjmDQ993ChII8SXWupQZVBiIpcWO4RqZk3lr7Bz5MUCwzDIA359e57SSq5CCkY0N\n\
+4B6Vulk7LktfwrdGNVI5BsC9qqxSwSKgRJeZ9wygIaehbHFHFhcBaMDKpiZlBHyz\n\
+rsnnlFXCb5s8HKn5LsUgGvB24L7sGNZP2CX7dhHov+YhD+jozLW2p9W4959Bz2Ei\n\
+RmqDtmiXLnzqTpXbI+suyCsohKRg6Un0RC47+cpiVwHiXZAW+cn8eiNIjqbVgXLx\n\
+KPpdzvvtTnOPlC7SQZSYmdunr3Bf9b77AiC/ZidstK36dRILKz7OA54=\n\
+-----END CERTIFICATE-----";
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial)
+    ;
+
+  WiFi.mode(WIFI_STA);
+  delay(500);
+
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  // attempt to connect to Wifi network
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+
+    // Connect to WPA/WPA2 network:
+    WiFi.begin(ssid, pwd);
+
+    delay(1000);
+  }
+  Serial.println("\nConnected!");
+}
+
+void loop() {
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if (client) {
+
+    client->setCACert(cert);
+
+    HTTPClient https;
+    Serial.println("[HTTPS] begin...\n");
+    if (https.begin(*client, "https://katla.cloud/api/data")) {
+
+      String contentType = "application/json; charset=utf-8";
+      String postData = "{ \"secret\": \"" + String(secret) + "\"," +
+                        "\"temp_in\":" + String(random(100) * 0.2) + "," +
+                        "\"temp_out\":" + String(random(100) * 0.1) + "," +
+                        "\"humidity\":" + String(random(100) * 0.4) + " }";
+
+      https.addHeader("Content-Type", contentType);
+      int statusCode = https.POST(postData);
+      String response = https.getString();
+
+      Serial.print("Status code: ");
+      Serial.println(statusCode);
+      Serial.print("Response: ");
+      Serial.println(response);
+
+      https.end();
+    } else {
+      Serial.println("[HTTPS] Connecting failed!");
+    }
+  }
+  delay(1000);
+}
