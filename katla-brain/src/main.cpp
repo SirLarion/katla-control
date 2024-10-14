a4e4056e0f7f31153ce8e19948c753c75c9aaad8
Jonas <jonas@tjepkema.com>
2024-10-14 13:16:11 +0300

    changed POST to katla server

--- katla-brain/src/main.cpp
+++ katla-brain/src/main.cpp
@@ -4,8 +4,8 @@
 #include <HTTPClient.h> 
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
-#include <ArduinoJson.h>
 #include <utils.h>
+#include <secrets.h>
 
 //JST PIN DEFINITIONS
 #define PIN1 D10 //DHT pin
@@ -32,11 +32,43 @@ float avgTemp_1;
 float avgTemp_2;
 
 //WIFI CONNECTION DEFINITIONS
-#define WIFI_SSID "aalto open"
-// #define WIFI_SSID "Luolan_Netti"
-// #define PSWD ""
-String servIP = "http://192.168.1.31:8000/";
-String serv_key = "C4Xw4LhlIu";
+bool home = 0;
+#define OPEN_SSID "aalto open"
+
+String serv = "https://katla.cloud/api/data";
+
+const char *secret = CONTROLLER_SECRET;
+WiFiClientSecure client;
+
+const char *cert = "-----BEGIN CERTIFICATE-----\n\
+MIIFBTCCAu2gAwIBAgIQS6hSk/eaL6JzBkuoBI110DANBgkqhkiG9w0BAQsFADBP\n\
+MQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJuZXQgU2VjdXJpdHkgUmVzZWFy\n\
+Y2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBYMTAeFw0yNDAzMTMwMDAwMDBa\n\
+Fw0yNzAzMTIyMzU5NTlaMDMxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBF\n\
+bmNyeXB0MQwwCgYDVQQDEwNSMTAwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\n\
+AoIBAQDPV+XmxFQS7bRH/sknWHZGUCiMHT6I3wWd1bUYKb3dtVq/+vbOo76vACFL\n\
+YlpaPAEvxVgD9on/jhFD68G14BQHlo9vH9fnuoE5CXVlt8KvGFs3Jijno/QHK20a\n\
+/6tYvJWuQP/py1fEtVt/eA0YYbwX51TGu0mRzW4Y0YCF7qZlNrx06rxQTOr8IfM4\n\
+FpOUurDTazgGzRYSespSdcitdrLCnF2YRVxvYXvGLe48E1KGAdlX5jgc3421H5KR\n\
+mudKHMxFqHJV8LDmowfs/acbZp4/SItxhHFYyTr6717yW0QrPHTnj7JHwQdqzZq3\n\
+DZb3EoEmUVQK7GH29/Xi8orIlQ2NAgMBAAGjgfgwgfUwDgYDVR0PAQH/BAQDAgGG\n\
+MB0GA1UdJQQWMBQGCCsGAQUFBwMCBggrBgEFBQcDATASBgNVHRMBAf8ECDAGAQH/\n\
+AgEAMB0GA1UdDgQWBBS7vMNHpeS8qcbDpHIMEI2iNeHI6DAfBgNVHSMEGDAWgBR5\n\
+tFnme7bl5AFzgAiIyBpY9umbbjAyBggrBgEFBQcBAQQmMCQwIgYIKwYBBQUHMAKG\n\
+Fmh0dHA6Ly94MS5pLmxlbmNyLm9yZy8wEwYDVR0gBAwwCjAIBgZngQwBAgEwJwYD\n\
+VR0fBCAwHjAcoBqgGIYWaHR0cDovL3gxLmMubGVuY3Iub3JnLzANBgkqhkiG9w0B\n\
+AQsFAAOCAgEAkrHnQTfreZ2B5s3iJeE6IOmQRJWjgVzPw139vaBw1bGWKCIL0vIo\n\
+zwzn1OZDjCQiHcFCktEJr59L9MhwTyAWsVrdAfYf+B9haxQnsHKNY67u4s5Lzzfd\n\
+u6PUzeetUK29v+PsPmI2cJkxp+iN3epi4hKu9ZzUPSwMqtCceb7qPVxEbpYxY1p9\n\
+1n5PJKBLBX9eb9LU6l8zSxPWV7bK3lG4XaMJgnT9x3ies7msFtpKK5bDtotij/l0\n\
+GaKeA97pb5uwD9KgWvaFXMIEt8jVTjLEvwRdvCn294GPDF08U8lAkIv7tghluaQh\n\
+1QnlE4SEN4LOECj8dsIGJXpGUk3aU3KkJz9icKy+aUgA+2cP21uh6NcDIS3XyfaZ\n\
+QjmDQ993ChII8SXWupQZVBiIpcWO4RqZk3lr7Bz5MUCwzDIA359e57SSq5CCkY0N\n\
+4B6Vulk7LktfwrdGNVI5BsC9qqxSwSKgRJeZ9wygIaehbHFHFhcBaMDKpiZlBHyz\n\
+rsnnlFXCb5s8HKn5LsUgGvB24L7sGNZP2CX7dhHov+YhD+jozLW2p9W4959Bz2Ei\n\
+RmqDtmiXLnzqTpXbI+suyCsohKRg6Un0RC47+cpiVwHiXZAW+cn8eiNIjqbVgXLx\n\
+KPpdzvvtTnOPlC7SQZSYmdunr3Bf9b77AiC/ZidstK36dRILKz7OA54=\n\
+-----END CERTIFICATE-----";
 
 //TIMING LOOP DEFINITIONS
 //Timer for the section that verifies new inputs through GET request
@@ -59,15 +91,28 @@ void setup() {
 
   dht.begin();
 
-  Serial.print("Connecting to Wifi SSID ");
-  Serial.print(WIFI_SSID);
-  WiFi.begin(WIFI_SSID);
-  // WiFi.begin(WIFI_SSID, PSWD);
+  WiFi.mode(WIFI_STA);
+  delay(500);
 
+  Serial.print("Attempting to connect to WPA SSID: ");
+  // attempt to connect to Wifi network
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(500);
+
+    // Connect to WPA/WPA2 network:
+    if(home){
+      Serial.println(SSID_SECRET);
+      WiFi.begin(SSID_SECRET, PSWD_SECRET);
+    }else{
+      Serial.println(OPEN_SSID);
+      WiFi.begin(OPEN_SSID);
+    }
+
+    delay(1000);
   }
+  Serial.println("\nConnected!");
+
+  client.setCACert(cert);
 
   //setting up the base temperarure reading
   avgTemp_1 = tmp36_1.read_C();
@@ -114,15 +159,28 @@ void loop() {
       Serial.println(analogReadMilliVolts(PINTHERM2));
 
       //SERVER POST
-      HTTPClient http;
-      http.begin(servIP);
-      http.addHeader("Content-Type", "application/json");
+      HTTPClient https;
+      if (https.begin(client, serv)) {
 
-      String data_json;
-      data_json = "{\"key\": \"" + serv_key + "\",\"sensor_data\": {\"DHT_hum\": " + hum + ", \"DHT_temp\": " + temp + ", \"TMP36_1\": " + avgTemp_1 + ", \"TMP36_2\": " + avgTemp_2 + "}}";
-      Serial.println(data_json);
-      int httpResponseCode = http.POST(data_json);
-      http.end();
+        String contentType = "application/json; charset=utf-8";
+        String postData = "{ \"secret\": \"" + String(secret) + "\"," +
+                          "\"temp_in\":" + String(avgTemp_1) + "," +
+                          "\"temp_out\":" + String(temp) + "," +
+                          "\"humidity\":" + String(hum) + " }";
+
+        https.addHeader("Content-Type", contentType);
+        int statusCode = https.POST(postData);
+        String response = https.getString();
+
+        Serial.print("Status code: ");
+        Serial.println(statusCode);
+        Serial.print("Response: ");
+        Serial.println(response);
+
+        https.end();
+      } else {
+        Serial.println("[HTTPS] Connecting failed!");
+      }
     }
     lastTime_POST = millis();
   }
